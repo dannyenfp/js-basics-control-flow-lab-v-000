cf16dd9a1c8806108e580ff3f05a92394c0838fa
Daniel Valmas <daniel.valmas@gmail.com>
2018-08-17 18:58:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,9 +3,9 @@
 function scuberGreetingForFeet (someValue) {
   if (someValue <= 400) {
     return "This one is on me!";
-  } else if { someValue > 2000 && someValue <= 2500) 
+  } else if { ( someValue > 2000 && someValue <= 2500) 
     return "I will gladly take your thirty bucks!";
   } else (someValue > 2500) {
-    return 'No can do.';  
+    return "No can do.";  
   }
 }
