7ddad60578bc7e01ab5d453dc41a977c445988ce
Daniel Valmas <daniel.valmas@gmail.com>
2018-08-17 19:06:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,18 +11,9 @@ function scuberGreetingForFeet (someValue) {
 }
 
 function ternaryCheckCity(city) {
-  city = 'NYC' ? return "Ok, sounds good."; : return "No go.";
+  (city = 'NYC') ? return "Ok, sounds good." : return "No go.";
 }
 
-describe('ternaryCheckCity()', function () {
-    it('returns "Ok, sounds good." when the city is NYC', function () {
-      expect(ternaryCheckCity('NYC')).to.equal('Ok, sounds good.');
-    });
-
-    it('should return "No go." if the destination city is not NYC', function () {
-      expect(ternaryCheckCity('Pittsburgh')).to.equal('No go.');
-    });
-  });
 
 
 
